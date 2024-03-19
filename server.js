f170ce87a49804a9b39c7d8170bb948641a54b9d
subasis14 <subasis14@gmail.com>
2024-03-19 09:13:14 +0530

    intial upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Listing");
+});
